8697f545d6f4c744238d3b1664b0a6162e2691b0
NguyenNhutQuoc <nguyennhutquoc1412@gmail.com>
2022-05-09 10:06:49 +0700

    Delete semicolon in model file

--- model/model.js
+++ model/model.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
-const slug = require('mongoose-slug-generator');
+const mongoose = require('mongoose')
+const slug = require('mongoose-slug-generator')
 mongoose.plugin(slug)
-const CategoriesSchema = require('./Categories');
-const Products = require('./Products');
-const Users = require('./Users');
-const SizesSchema = require('./Sizes');
-const Colors = require('./Colors');
-const Coupons = require('./Coupons');
-const CommentsSchema = require('./Comments');
-const Bills = require('./Bills');
-const BillDetails = require('./BillDetails');
-const ProductDetails = require('./ProductDetails');
+const CategoriesSchema = require('./Categories')
+const Products = require('./Products')
+const Users = require('./Users')
+const SizesSchema = require('./Sizes')
+const Colors = require('./Colors')
+const Coupons = require('./Coupons')
+const CommentsSchema = require('./Comments')
+const Bills = require('./Bills')
+const BillDetails = require('./BillDetails')
+const ProductDetails = require('./ProductDetails')
 const ImagesSchema = require('./Images')
 module.exports = {
     CategoriesSchema,
