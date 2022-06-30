6a0506888790a33e1c7489e4544064e06e458d1a
NguyenNhutQuoc <nguyennhutquoc1412@gmail.com>
2022-06-30 21:34:57 +0700

    Done

--- controllers/ConversationController.js
+++ controllers/ConversationController.js
@@ -1,12 +1,12 @@
 const {
-    Conversation
+    Conversations
 } = require('../model/model');
 
 const ConversationController = {
 
     //[POST] /api/conversations
     createConversation: async (req, res) => {
-        const newConversation = new Conversation({
+        const newConversation = new Conversations({
             members: [req.body.senderId, req.body.receiverId],
         });
 
@@ -21,7 +21,7 @@ const ConversationController = {
     //[GET] /api/conversations/:userId
     getConversationOfUser: async (req, res) => {
         try {
-            const conversation = await Conversation.find({
+            const conversation = await Conversations.find({
                 members: { $in: [req.params.userId] },
             });
             res.status(200).json(conversation);
@@ -33,8 +33,10 @@ const ConversationController = {
     //[GET] /api/conversations/:firstUserId/:secondUserId
     getConversationOfTwoUser: async (req, res) => {
         try {
-            const conversation = await Conversation.findOne({
-                members: { $all: [req.params.firstUserId, req.params.secondUserId] },
+            const conversation = await Conversations.findOne({
+                $all: {
+                    members: [req.params.firstUserId, req.params.secondUserId]
+                }
             });
             res.status(200).json(conversation)
         } catch (err) {
